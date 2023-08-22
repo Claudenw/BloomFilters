89df32751ea52630e930154074916f37a7d3cd45
Claude Warren <claude.warren@aiven.io>
2023-08-22 19:59:12 +0200

    added tests, cleandup code

--- src/main/java/org/xenei/bloomfilter/segmented/ByteBufferBloomFilter.java
+++ src/main/java/org/xenei/bloomfilter/segmented/ByteBufferBloomFilter.java
@@ -1,12 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.xenei.bloomfilter.segmented;
 
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.LongBuffer;
 import java.util.function.IntPredicate;
 import java.util.function.LongPredicate;
 
-import org.apache.commons.collections4.bloomfilter.BitMap;
 import org.apache.commons.collections4.bloomfilter.BitMapProducer;
 import org.apache.commons.collections4.bloomfilter.BloomFilter;
 import org.apache.commons.collections4.bloomfilter.IndexProducer;
@@ -39,7 +52,8 @@ public class ByteBufferBloomFilter implements BloomFilter {
     }
 
     public ByteBufferBloomFilter(Shape shape, ByteBuffer buffer, int position) {
-        this.buffer = (buffer == null) ? ByteBuffer.allocate(numberOfBytes(shape.getNumberOfBits())) : (position == 0) ? buffer.duplicate() : buffer.slice();
+        this.buffer = (buffer == null) ? ByteBuffer.allocate(numberOfBytes(shape.getNumberOfBits()))
+                : (position == 0) ? buffer.duplicate() : buffer.slice();
         this.shape = shape;
         int limit = numberOfBytes(shape.getNumberOfBits());
         if (limit > this.buffer.capacity()) {
@@ -116,7 +130,7 @@ public class ByteBufferBloomFilter implements BloomFilter {
 
     private boolean forEachIndex(byte bb, int offset, IntPredicate predicate) {
         int b = bb & 0xFF;
-        int i=offset;
+        int i = offset;
         while (b != 0) {
             if ((b & 1) == 1 && !predicate.test(i)) {
                 return false;
@@ -126,25 +140,24 @@ public class ByteBufferBloomFilter implements BloomFilter {
         }
         return true;
     }
-    
+
     @Override
     public boolean forEachIndex(IntPredicate predicate) {
         int limit = numberOfBytes(shape.getNumberOfBits());
         for (int idx = 0; idx < limit; idx++) {
-            if (!forEachIndex(buffer.get(idx), idx*8, predicate)) {
+            if (!forEachIndex(buffer.get(idx), idx * 8, predicate)) {
                 return false;
             }
         }
         return true;
     }
-    
 
     @Override
     public boolean forEachBitMap(LongPredicate predicate) {
         long result = 0;
         int shift = 0;
-        for (int i=0;i<buffer.limit();i++) {
-            long b = buffer.get(i) & 0xFF; 
+        for (int i = 0; i < buffer.limit(); i++) {
+            long b = buffer.get(i) & 0xFF;
             long x = b << shift;
             result |= (b << shift);
             shift += Byte.SIZE;
@@ -163,13 +176,13 @@ public class ByteBufferBloomFilter implements BloomFilter {
         ByteBufferBloomFilter result = new ByteBufferBloomFilter(shape);
         int numberOfBytes = numberOfBytes(shape.getNumberOfBits());
         if (this.buffer.hasArray()) {
-            result.buffer.put( this.buffer.array(), this.buffer.arrayOffset(), numberOfBytes );
+            result.buffer.put(this.buffer.array(), this.buffer.arrayOffset(), numberOfBytes);
         } else {
-            byte[] xfer = new byte[1024*1024];
-            int offset=0;
+            byte[] xfer = new byte[1024 * 1024];
+            int offset = 0;
             int limit;
             while (offset < result.buffer.limit()) {
-                limit = (offset+xfer.length > result.buffer.limit()) ? result.buffer.limit() : offset+xfer.length;
+                limit = (offset + xfer.length > result.buffer.limit()) ? result.buffer.limit() : offset + xfer.length;
                 this.buffer.get(xfer, offset, limit);
                 result.buffer.put(xfer, offset, limit);
                 offset += limit;
@@ -205,37 +218,37 @@ public class ByteBufferBloomFilter implements BloomFilter {
     public boolean merge(IndexProducer indexProducer) {
         cardinality = -1;
         return indexProducer.forEachIndex(i -> {
-            if (i >-1 && i < shape.getNumberOfBits()) {
+            if (i > -1 && i < shape.getNumberOfBits()) {
                 int idx = getByteIndex(i);
                 buffer.put(idx, (byte) (buffer.get(idx) | getIntBit(i)));
                 return true;
             } else {
-                throw new IllegalArgumentException( 
-                        String.format("Bloom filter only accepts indexes in the range [0..%s)", shape.getNumberOfBits()));
+                throw new IllegalArgumentException(String
+                        .format("Bloom filter only accepts indexes in the range [0..%s)", shape.getNumberOfBits()));
             }
         });
     }
 
     @Override
     public boolean merge(BitMapProducer bitMapProducer) {
-        //return merge(IndexProducer.fromBitMapProducer(bitMapProducer));
+        // return merge(IndexProducer.fromBitMapProducer(bitMapProducer));
         int idx[] = new int[] { 0 };
         cardinality = -1;
-        return bitMapProducer.forEachBitMap( l -> {
+        return bitMapProducer.forEachBitMap(l -> {
             int bufferIdx;
             int shift;
-            for (int i=0; i< Long.BYTES; i++) {
-                shift = i*Byte.SIZE;
+            for (int i = 0; i < Long.BYTES; i++) {
+                shift = i * Byte.SIZE;
                 long mask = 0xFFL << shift;
                 if ((l & mask) != 0) {
                     bufferIdx = idx[0] + i;
                     if (bufferIdx >= buffer.limit()) {
                         throw new IllegalArgumentException(
-                            String.format("BitMapProducer set a bit higher than the limit for the shape: %s",
-                                    shape.getNumberOfBits()));
+                                String.format("BitMapProducer set a bit higher than the limit for the shape: %s",
+                                        shape.getNumberOfBits()));
                     }
-                    long b = (buffer.get(bufferIdx) & 0xFF) |(((l & mask) >> shift) & 0xFF);
-                    buffer.put(bufferIdx, (byte)b);
+                    long b = (buffer.get(bufferIdx) & 0xFF) | (((l & mask) >> shift) & 0xFF);
+                    buffer.put(bufferIdx, (byte) b);
                 }
             }
             idx[0] += Long.BYTES;
--- src/test/java/org/xenei/bloomfilter/segmented/BitMapProducerFromByteBufferBloomFilterTest.java
+++ src/test/java/org/xenei/bloomfilter/segmented/BitMapProducerFromByteBufferBloomFilterTest.java
@@ -16,36 +16,26 @@
  */
 package org.xenei.bloomfilter.segmented;
 
-import org.apache.commons.collections4.bloomfilter.AbstractIndexProducerTest;
+import org.apache.commons.collections4.bloomfilter.AbstractBitMapProducerTest;
+import org.apache.commons.collections4.bloomfilter.BitMapProducer;
+import org.apache.commons.collections4.bloomfilter.Hasher;
 import org.apache.commons.collections4.bloomfilter.IncrementingHasher;
-import org.apache.commons.collections4.bloomfilter.IndexProducer;
 import org.apache.commons.collections4.bloomfilter.Shape;
 
-public class IndexProducerFromByteBufferBloomFilterTest extends AbstractIndexProducerTest {
+public class BitMapProducerFromByteBufferBloomFilterTest extends AbstractBitMapProducerTest {
 
     protected Shape shape = Shape.fromKM(17, 72);
 
     @Override
-    protected IndexProducer createProducer() {
-        ByteBufferBloomFilter filter = new ByteBufferBloomFilter(shape);
-        filter.merge(new IncrementingHasher(0, 1));
-        filter.merge(new IncrementingHasher(5, 1));
-        return filter;
+    protected BitMapProducer createProducer() {
+        final Hasher hasher = new IncrementingHasher(0, 1);
+        final ByteBufferBloomFilter bf = new ByteBufferBloomFilter(shape);
+        bf.merge(hasher);
+        return bf;
     }
 
     @Override
-    protected IndexProducer createEmptyProducer() {
+    protected BitMapProducer createEmptyProducer() {
         return new ByteBufferBloomFilter(shape);
     }
-
-    @Override
-    protected int getAsIndexArrayBehaviour() {
-        return DISTINCT | ORDERED;
-    }
-
-    @Override
-    protected int[] getExpectedIndices() {
-        return new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
-    }
-
 }
